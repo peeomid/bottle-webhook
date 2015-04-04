b871a5db8d3859b68f03bcb8a7e8c4065c191d61
Luan Nguyen Thanh <luan.nguyen@rainmaker-labs.com>
2015-04-04 16:46:36 +0700

    demon :)

--- hook.py
+++ hook.py
@@ -1,13 +1,15 @@
 from bottle import route, run, get
+from bottledaemon import daemon_run
 import subprocess
 import logging
 import os
 
 LOG_FILE = 'hook.log'
+LOG_PATH = os.path.dirname(os.path.realpath(__file__)) + '/' + LOG_FILE
 
 @get('/pull/<path:path>')
 def pull(path):
-    logging.basicConfig(filename=LOG_FILE, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.INFO)
+    logging.basicConfig(filename=LOG_PATH, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.INFO)
     logging.info('======================')
     logging.info('Pulling for %s' % path)
     path = path if path.startswith('/') else '/' + path
@@ -21,4 +23,6 @@ def pull(path):
     logging.info('given path %s does not exist' % path)
     return 'Given path does not exist, dude! Give me a proper path to work with lah! :)'
 
-run(host='localhost', port=8989)
+# run(host='localhost', port=8989)
+if __name__ == "__main__":
+  daemon_run(host='localhost', port=8989)
